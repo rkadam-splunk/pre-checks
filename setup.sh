ed0dcaa9851582afd29851d07e0d6cd284542f38
akrishnakumar-splunk <akrishnakumar@splunk.com>
2018-04-18 11:48:05 +0530

    Documentation improvements

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
-sudo pip install jira --ignore-installed six
-sudo pip install confluence-py --ignore-installed six
-sudo pip install dnspython --ignore-installed six
-sudo pip install beautifulsoup4 --ignore-installed six
-sudo pip install beautifulsoup --ignore-installed six
-sudo pip install -U --user PyOpenSSL
+#!/bin/bash
+
+pip -V
+retval=$?
+do_something $retval
+if [ $retval -ne 0 ]; then
+    echo "Something is wrong : $retval"
+    echo "\n --- Looks like pip isn't installed on your machine... Installing pip --- \n"
+    sudo easy_install pip
+else
+    sudo pip install jira confluence-py dnspython beautifulsoup4 beautifulsoup --ignore-installed six
+    sudo pip install -U --user PyOpenSSL
+    echo "\n --- Good to Go! --- \n"
+fi
