b4ab097a6eb1584863cc956dcbbc1dff85b05874
Sanket Bhimani <35288087+sbhimani-splunk@users.noreply.github.com>
2018-04-23 14:07:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
+sudo python get-pip.py
 sudo pip install jira --ignore-installed six
 sudo pip install confluence-py --ignore-installed six
 sudo pip install dnspython --ignore-installed six
