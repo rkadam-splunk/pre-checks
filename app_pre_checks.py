2d3d192688a009236ca13ba7e3ac9edec36f7865
sanket bhimani <sbhimani@splunk.com>
2018-05-27 20:50:47 +0530

    small changes

--- app_pre_checks.py
+++ app_pre_checks.py
@@ -477,6 +477,7 @@ def main():
 							print "\t*Automation failed to find APPCERT JIRA for dependent app",_id+"*"
 			else:
 				print "*dependent app is not available on confluence page*"
+		print ""
 
 	if appcert_flg == 1:
 		print "Gone through below JIRA to get the remaining APPCERT JIRA"
